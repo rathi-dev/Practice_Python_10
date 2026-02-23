04eafce670fef9353da2664d41b4fe86f4097786
kejaashree <kejaashreekesavan@gmail.com>
2026-02-23 19:38:07 +0530

    Add files via upload

---
+++ graph_xy.py
@@ -0,0 +1,9 @@
+x,y=int(input("x=")),int(input("y="))
+if x<0 and y<0 :
+    print("it plot at 3nd quadrant")
+elif x>0 and y>0 :
+    print("it plot at 1st quadrant")
+elif x<0 and y>0 :
+    print("it plot at 2nd quadrant")
+else:
+    print("it plot at 4th quadrant")
